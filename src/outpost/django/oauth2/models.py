0a0c338f11c1da8870b64e8b04bcdf83deb1d8d5
Michael Fladischer <michael@fladi.at>
2022-03-02 13:11:42 +0100

    Add group support to applications.

--- src/outpost/django/oauth2/models.py
+++ src/outpost/django/oauth2/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import URLValidator
 from django.db import models
+from django.contrib.auth.models import Group
 from oauth2_provider.models import AbstractApplication
 
 
@@ -9,6 +10,12 @@ class Application(AbstractApplication):
     website = models.TextField(validators=[URLValidator()])
     privacy = models.TextField(validators=[URLValidator()])
     description = models.TextField()
+    groups = models.ManyToManyField(Group, blank=True)
 
     class Meta(AbstractApplication.Meta):
         pass
+
+    def is_usable(self, request):
+        if not self.groups.exists():
+            return True
+        return self.groups.intersection(self.user.groups.all()).exists()
